fc51a9c5ee22d5c6736d981c148a5c640f5bda80
Yoltic2 <eyolticmg@icloud.com>
2022-05-31 22:38:06 -0500

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Emilio Y Martinez on 31/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
